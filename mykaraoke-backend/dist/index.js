252b3c6e5fe83680cd38daf968b635fbf5de953c
Frank Lee <flee63@gatech.edu>
2023-08-08 00:46:54 -0400

    commit

--- mykaraoke-backend/dist/index.js
+++ mykaraoke-backend/dist/index.js
@@ -1,11 +1,9 @@
 import express from "express";
 const app = express();
 const port = 3000;
-
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.send("Hello, blue!");
 });
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
