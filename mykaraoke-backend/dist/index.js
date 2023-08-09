67ec39d75cbc7c2623131a1f81ca99bbe20fad46
Frank Lee <flee63@gatech.edu>
2023-08-09 09:03:20 -0400

    update backend to pg

--- mykaraoke-backend/dist/index.js
+++ mykaraoke-backend/dist/index.js
@@ -1,9 +1,31 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 import express from "express";
+import cors from "cors";
+import pkg from "pg";
+const { Client } = pkg;
+import dotenv from "dotenv";
+dotenv.config();
+const client = new Client({
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+await client.connect();
 const app = express();
 const port = 3000;
-app.get("/", (req, res) => {
-    res.send("Hello, blue!");
-});
+app.use(cors());
+app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const results = yield client.query("SELECT * FROM users;");
+    res.json(results.rows);
+}));
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
--- mykaraoke-backend/src/index.ts
+++ mykaraoke-backend/src/index.ts
@@ -1,10 +1,25 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
+import pkg from "pg";
+const { Client } = pkg;
+import dotenv from "dotenv";
+dotenv.config();
+
+const client = new Client({
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+await client.connect();
 
 const app = express();
 const port = 3000;
+app.use(cors());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, blue!");
+app.get("/", async (req: Request, res: Response) => {
+  const results = await client.query("SELECT * FROM users;");
+  res.json(results.rows);
 });
 
 app.listen(port, () => {
