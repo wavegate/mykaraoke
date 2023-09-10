026243593b52a41a2e6c114decb84afe27125ed0
ehuang47 <44561409+ehuang47@users.noreply.github.com>
2023-09-10 19:29:00 -0400

    fixed router issue with storybook stories

--- mykaraoke/.storybook/main.ts
+++ mykaraoke/.storybook/main.ts
@@ -11,6 +11,7 @@ const config: StorybookConfig = {
       name: '@storybook/addon-styling',
       options: {},
     },
+    'storybook-addon-react-router-v6'
   ],
   framework: {
     name: '@storybook/react-vite',
