5b12dc71a9c45fdca6d8ba9c844c25e430297704
ehuang47 <44561409+ehuang47@users.noreply.github.com>
2023-09-10 20:32:30 -0400

    expanding roles page to be full-width
    Added optional props to AnimatedPage for position absolute property

--- mykaraoke/src/components/AnimatedPage/AnimatedPage.tsx
+++ mykaraoke/src/components/AnimatedPage/AnimatedPage.tsx
@@ -2,18 +2,22 @@ import { motion } from "framer-motion";
 import { ReactNode } from "react";
 
 interface IAnimatedPage {
-  children: ReactNode;
+	children: ReactNode;
+	positionAbsolute?: boolean;
 }
 
-export default function AnimatedPage({ children }: IAnimatedPage) {
-  return (
-    <motion.div
-      className={`absolute p-4`}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-    >
-      {children}
-    </motion.div>
-  );
+export default function AnimatedPage({
+	children,
+	positionAbsolute = true,
+}: IAnimatedPage) {
+	return (
+		<motion.div
+			className={`${positionAbsolute ? "absolute" : ""} p-4`}
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			exit={{ opacity: 0 }}
+		>
+			{children}
+		</motion.div>
+	);
 }
--- mykaraoke/src/pages/RolesPage.tsx
+++ mykaraoke/src/pages/RolesPage.tsx
@@ -7,13 +7,15 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function RolesPage() {
 	return (
-		<AnimatedPage>
+		<AnimatedPage positionAbsolute={false}>
 			<div>Search for Roles</div>
-			<div className="grid w-full max-w-sm items-center gap-1.5">
+			<div className="grid w-full items-center gap-1.5">
 				<Label htmlFor="search">Search</Label>
-				<Input type="text" id="search" placeholder="Search for roles" />
-				<FontAwesomeIcon icon={faMagnifyingGlass} />
-				<Button type="button">Clear Filters</Button>
+				<div className="flex items-center space-x-2">
+					<FontAwesomeIcon icon={faMagnifyingGlass} />
+					<Input type="text" id="search" placeholder="Search for roles" />
+					<Button type="button">Clear Filters</Button>
+				</div>
 			</div>
 		</AnimatedPage>
 	);
