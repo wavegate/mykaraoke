5d5cb9be3cda25d9e85f720c66ce20b69b5093af
Frank Lee <flee63@gatech.edu>
2023-10-02 21:29:12 -0400

    commit

--- mykaraoke-backend/dist/routes/authRouter.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { login, register, user } from "../controllers/authController.js";
-import verifyToken from "../middleware/verifyToken.js";
-const router = express.Router();
-router.post("/login", login);
-router.post("/register", register);
-router.get("/user", verifyToken, user);
-const authRouter = router;
-export default authRouter;
