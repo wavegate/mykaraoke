89dcfb573deda630798f7c2ec628c25144b5ba96
ehuang47 <44561409+ehuang47@users.noreply.github.com>
2023-10-08 16:19:00 -0400

    feat: added dropdownMenuCheckbox

--- mykaraoke/src/pages/RolesPage.tsx
+++ mykaraoke/src/pages/RolesPage.tsx
@@ -1,5 +1,5 @@
 // "use client";
-import { useState } from "react";
+import DropdownMenuCheckbox from "@/components/DropdownMenuCheckbox/DropdownMenuCheckbox";
 import AnimatedPage from "@/components/AnimatedPage/AnimatedPage";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -24,12 +24,12 @@ export default function RolesPage() {
 					<Button type="button">Clear Filters</Button>
 				</div>
 				<div className="flex gap-2">
-					<MyDropdownMenu
+					<DropdownMenuCheckbox
 						title="Category"
 						icon={<FontAwesomeIcon icon={faCubesStacked} />}
 						options={["Software Engineering", "Product Design", "Product Management"]}
 					/>
-					<MyDropdownMenu
+					<DropdownMenuCheckbox
 						title="Education"
 						icon={<FontAwesomeIcon icon={faGraduationCap} />}
 						options={[
@@ -42,7 +42,7 @@ export default function RolesPage() {
 							"PharmD",
 						]}
 					/>
-					<MyDropdownMenu
+					<DropdownMenuCheckbox
 						title="Location"
 						icon={<FontAwesomeIcon icon={faLocationDot} />}
 						options={["Remote", "Remote in USA", "Hybrid"]}
@@ -50,57 +50,5 @@ export default function RolesPage() {
 				</div>
 			</div>
 		</AnimatedPage>
-	);
-}
-
-// import { Button } from "@/components/ui/button";
-// import { DropdownMenuCheckboxItemProps } from "@radix-ui/react-dropdown-menu";
-import {
-	DropdownMenu,
-	DropdownMenuCheckboxItem,
-	DropdownMenuContent,
-	DropdownMenuLabel,
-	DropdownMenuSeparator,
-	DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-
-// type Checked = DropdownMenuCheckboxItemProps["checked"];
-
-function MyDropdownMenu({ title, icon, options }) {
-	const [selectedOptions, setSelectedOptions] = useState<Record<string, boolean>>(
-		options.reduce((map, option) => {
-			map[option] = false;
-			return map;
-		}, {})
-	);
-
-	const handleSelectedOption = (option) => () => {
-		setSelectedOptions((prev) => {
-			const newOptions = { ...prev, [option]: !prev[option] };
-			return newOptions;
-		});
-	};
-
-	return (
-		<DropdownMenu>
-			<DropdownMenuTrigger asChild>
-				<Button variant="outline" className="gap-2">
-					{icon}
-					{title}
-				</Button>
-			</DropdownMenuTrigger>
-			<DropdownMenuContent className="w-56">
-				<DropdownMenuLabel>{title}</DropdownMenuLabel>
-				<DropdownMenuSeparator />
-				{options.map((option) => (
-					<DropdownMenuCheckboxItem
-						checked={selectedOptions[option]}
-						onCheckedChange={handleSelectedOption(option)}
-					>
-						{option}
-					</DropdownMenuCheckboxItem>
-				))}
-			</DropdownMenuContent>
-		</DropdownMenu>
 	);
 }
