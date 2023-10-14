f327e1911e89b1a149c74ed385ec918f65af6fb8
Frank Lee <flee63@gatech.edu>
2023-10-14 09:45:59 -0400

    commit

--- mykaraoke-backend/convert_pdf_to_docx.py
+++ mykaraoke-backend/convert_pdf_to_docx.py
@@ -2,14 +2,17 @@ import sys
 import os
 from pdf2docx import Converter
 
-pdf_path = sys.argv[1]
+try:
+    pdf_path = sys.argv[1]
 
-output_path = os.path.splitext(pdf_path)[0] + ".docx"
+    output_path = os.path.splitext(pdf_path)[0] + ".docx"
 
-print(output_path)
+    print(output_path)
 
-converter = Converter(pdf_path)
-converter.convert(output_path)
-converter.close()
+    converter = Converter(pdf_path)
+    converter.convert(output_path)
+    converter.close()
 
-print("Conversion completed")
+    print("Conversion completed")
+except Exception as e:
+    print(e)
